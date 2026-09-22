b8333c1996cc077d8a76315155a1e320c68a9cf2
imrohitsengar <rsengar257@gmail.com>
2026-09-22 22:58:03 +0530

    Assignment Done

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,30 @@
+import React, { useState } from "react";
+import "../styles/Child.css";
 
+export default function Selection({ applyColor }) {
+  const [style, setStyle] = useState({ background: "" });
+
+  const handleClick = () => {
+    applyColor(setStyle);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{
+        background: style.background,
+        borderStyle: "solid",
+        borderWidth: "3px",
+        borderRadius: "10px",
+        color: "white",
+        fontSize: "1rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      onClick={handleClick}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
